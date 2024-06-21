42076271f83a5e5fb152236f2e4715e186ac2a38
Reghardt <reghardt1000@gmail.com>
2024-06-21 11:33:29 +0200

    LoRa submodule

--- pico_sdk_sensor.cpp
+++ pico_sdk_sensor.cpp
@@ -14,21 +14,13 @@
 #define ECHO_PIN 16
 #define MAX_DISTANCE 450
 
-#define I2C_SDA_PIN 2
-#define I2C_SCL_PIN 3
-
 LoRaClass loraClass = LoRaClass();
 NewPing sonar = NewPing(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-
-bool reserved_addr(uint8_t addr)
-{
-    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
-}
 
 int main()
 {
     stdio_init_all();
-    printf("Started 2.\n");
+    printf("Started\n");
 
     if (watchdog_caused_reboot())
     {
@@ -49,13 +41,7 @@ int main()
 
     gpio_set_function(PICO_DEFAULT_LED_PIN, GPIO_FUNC_SIO);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-
-    i2c_init(i2c1, 100 * 1000);
-    gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA_PIN);
-    gpio_pull_up(I2C_SCL_PIN);
-    bi_decl(bi_2pins_with_func(I2C_SDA_PIN, I2C_SCL_PIN, GPIO_FUNC_I2C));
+    gpio_put(PICO_DEFAULT_LED_PIN, GPIO_OFF);
 
     while (true)
     {
@@ -75,35 +61,6 @@ int main()
         loraClass.print('3');
         loraClass.print('4');
         loraClass.endPacket();
-
-        printf("\nI2C Bus Scan\n");
-        printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
-
-        for (int addr = 0; addr < (1 << 7); ++addr)
-        {
-            watchdog_update();
-            if (addr % 16 == 0)
-            {
-                printf("%02x ", addr);
-            }
-
-            // Perform a 1-byte dummy read from the probe address. If a slave
-            // acknowledges this address, the function returns the number of bytes
-            // transferred. If the address byte is ignored, the function returns
-            // -1.
-
-            // Skip over any reserved addresses.
-            int ret;
-            uint8_t rxdata;
-            if (reserved_addr(addr))
-                ret = PICO_ERROR_GENERIC;
-            else
-                ret = i2c_read_blocking(i2c1, addr, &rxdata, 1, false);
-
-            printf(ret < 0 ? "." : "@");
-            printf(addr % 16 == 15 ? "\n" : "  ");
-        }
-        printf("Done.\n");
     }
 }
 
